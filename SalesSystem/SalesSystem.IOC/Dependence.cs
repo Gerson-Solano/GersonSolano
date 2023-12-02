72468aad3fa61605d6a9a8c84494bc747479532d
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2023-12-01 23:47:18 -0600

    Add new project
    SalesSystem
    gitignore creation

---
+++ SalesSystem/SalesSystem.IOC/Dependence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SalesSystem.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SalesSystem.IOC
+{
+    public static class Dependence
+    {
+    }
+}
