5e873ebb9378ecb5004fa1ad6438857c9ea1c28f
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2023-12-16 14:53:06 -0600

    add changes
    feat: new nodeapiproject

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using ChallengeApp.models;
+namespace ChallengeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            challenge challenge = new challenge();
+            // Crear una instancia de Person utilizando el constructor
+            //Person p = new Person(1, "Gerson", "Solano", new DateTime(2000, 10, 23));
+
+            // challenge.MultiplyWithoutOperator(3, 6);
+            //challenge.TenPrimeNumbers();
+            int[] newArray = { 18, 2, 6, 9, 15, 3, 7, 4, 11 };
+            challenge.MaxandMinFromArray(newArray);
+            // Utilizar el método ToString() para imprimir información sobre la persona
+            //Console.WriteLine(p.ToString());
+            // Console.WriteLine(challenge.isPalindromo("A casaca"));
+        }
+    }
+    
+    
+    
+    
+    //Crear una funci6n que copie todas Ias propiedades y sus valores de un objeto en Otrol
+
+
+    class challenge
+    {
+        // 1- Write a funtion that detected if the string is a palindromo: "a casaca".
+        public bool isPalindromo(String pharse)
+        {
+           
+            String[] text = pharse.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            String tempTxt = "";
+            foreach (string letra in text)
+            {
+                Console.WriteLine("Letra: "+letra);
+                tempTxt += string.Join("", letra);
+            }
+            string resultado = string.Join("", pharse.Split(' '));
+            
+            if (resultado.ToLower().Equals(tempTxt.ToLower()))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        //Multiplicar dos nümeros sin utilizar el sfrnbolo de multiplicaci6n o de divisi6n.
+        public void MultiplyWithoutOperator(int a, int b)
+        {
+            var maxNumber = 0;
+            var minNumber = 0;
+            var result = 0;
+            if (a >= b)
+            {
+                maxNumber = a;
+                minNumber = b;
+            }
+            else
+            {
+                maxNumber = b;
+                minNumber = a;
+            }
+            for (int i = 0; i < maxNumber; i++)
+            {
+                result += minNumber;
+            }
+            Console.WriteLine(a+" * "+b+" es igual a: "+result);
+        }
+
+        //Imprimir los primeros 10 nümeros primos
+        public void TenPrimeNumbers()
+        {
+            for (int i = 2; i < 30; i++)
+            {
+                if (i/i == 1 && (i%3!=0 && i % 2 != 0) )
+                {
+                    Console.WriteLine(i);
+
+                    Console.WriteLine(i % i);
+                }
+            }
+        }
+
+        public void MaxandMinFromArray(int[] newArray)
+        {
+            int max = 0;
+            int min = 0;
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    max = newArray[i];
+                    min = newArray[i];
+                }
+                else{
+
+                    if (max<i)
+                    {
+                        max = newArray[i];
+                    }
+                    else if (min>i){
+                        min = newArray[i];
+                    }
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
+
+
+
---
+++ ChallengeApp/ChallengeApp/models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp.models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+       // Constructor que acepta parámetros para inicializar las propiedades
+            public Person(int id, string name, string lastName, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+
+        // Método ToString() para obtener una representación de cadena de la persona
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Last Name: {LastName}, Date of Birth: {DateOfBirth.ToShortDateString()}";
+        }
+    }
+}
