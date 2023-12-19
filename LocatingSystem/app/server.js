f7c9157b53d91fbcb989d60406dec50dc29cf341
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2023-12-19 14:10:20 -0600

    add my porfolio
    feat: create my gerson porfolio with astro

--- LocatingSystem/app/server.js
+++ LocatingSystem/app/server.js
@@ -2,3 +2,12 @@ const express = require("express")
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Executing Web Server');
+})
+
+const PORT = process.env.PORT || 2300;
+
+app.listen(PORT, ()=>{console.log('SUCCESS port server', PORT)});
